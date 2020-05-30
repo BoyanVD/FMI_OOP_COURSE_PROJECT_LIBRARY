a426f39ba82712fcb79da4ab2c25f6c80a1a4d73
BoyanVD <46243260+BoyanVD@users.noreply.github.com>
2020-05-30 14:22:54 +0300

    Main page documentation added

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,20 @@
 #include "Book.h"
 #include "Application.h"
 
+/**
+ * \mainpage Project Description
+ * 
+ * \section Project idea
+ *  This is a project, that implements functionality for user library management. It works with books and users, where
+ * books files can be dynamically changed from user throughout application runtime. Users file is static and it's always
+ * the same for the application. There are two types of users - admins and regular users. The admin users can use more
+ * of the application's commands, supported. User can filter, search, get information abouts books from file chosen.
+ * Admins can add and delete other users.
+ * 
+ * \section Github link :
+ * https://github.com/BoyanVD/FMI_OOP_COURSE_PROJECT_LIBRARY
+ */
+
 int main() 
 {
     Book::generateBooksFile("./files/books.bin");
