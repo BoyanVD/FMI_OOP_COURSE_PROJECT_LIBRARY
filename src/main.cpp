2d1e91fe5e2597a75af31fe7cac97bbb5b8608ab
BoyanVD <46243260+BoyanVD@users.noreply.github.com>
2020-05-30 14:25:38 +0300

    Small documentation changes

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,14 @@
 /**
  * \mainpage Project Description
  * 
- * \section Project idea
+ * \section Idea
  *  This is a project, that implements functionality for user library management. It works with books and users, where
  * books files can be dynamically changed from user throughout application runtime. Users file is static and it's always
  * the same for the application. There are two types of users - admins and regular users. The admin users can use more
  * of the application's commands, supported. User can filter, search, get information abouts books from file chosen.
  * Admins can add and delete other users.
  * 
- * \section Github link :
+ * \section Github
  * https://github.com/BoyanVD/FMI_OOP_COURSE_PROJECT_LIBRARY
  */
 
